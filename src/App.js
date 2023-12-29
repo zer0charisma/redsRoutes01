38a376256085868649aaf207ae5fecaddf55dd65
zer0charisma <natalie.d.rees@gmail.com>
2023-12-29 17:15:08 -0600

    Added Artwork button and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [artworks, setArtworks] = useState([]);
 
-export default App;
+    useEffect(() => { 
+      fetch("https://api.artic.edu/api/v1/artworks")
+      .then((response) => response.json())
+      .then(setArtworks)
+      .catch((error) => {
+        console.log(error);
+      });
+    }, []);
+
+    return (
+      <>
+      <ArtButton />
+      <ul> 
+        {artworks.map((art, index) => <ArtButton art={art} key={index} />)}
+      </ul>
+      </>
+    )
+};
+
+function ArtButton({art, key}) { 
+  const [clicked, setClicked] = useState(false);
+  const [imgUrl, setImgUrl] = useState('');
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  }
+
+  useEffect(() => {
+    if (clicked) {
+      const getAlbumCover = async () => {
+        const artId = art.id;
+        await fetch(`https://api.artic.edu/api/v1/artworks/${artId}?fields=id,title,image_id`)
+        .then((response) => response.json())
+        .then((res) => {
+          const image_id = res.data.image_id;
+          const iiif_url = res.config.iiif_url;
+          setImgUrl(`${iiif_url}/${image_id}/full/200,/0/default.jpg`);
+        })
+      };
+      getAlbumCover();
+    }
+  },[clicked, art.id]);
+
+  return (
+    <li key={key}>
+      <button onClick={handleClick}>Click Me</button>
+      {clicked ? <img src={imgUrl} alt={art.title}/> : <></>}
+    </li>
+
+  );
+  }
+
+  export default App;
